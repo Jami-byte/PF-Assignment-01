55de081cdd7f22c5b0c514cb61027826f5d1886a
Jami <ai.insights767@gmail.com>
2024-10-18 16:39:19 +0500

    Add files via upload

---
+++ Assignment Q02.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int number;
+    int square;
+
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+
+    square = number * number;
+
+    printf("The square of %d is %d\n", number, square);
+
+    return 0;
+}
